7189eea49f9452a2d3dc9cc149256358e602a79f
Maksim Voropay <muckam49@mail.ru>
2017-02-13 17:39:33 +0800

    небольш. дораб.

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,47 @@
-var buttonOpen=document.querySelector(".main-contacts .btn"),modal=document.querySelector(".feedback-form"),modalBackground=document.querySelector(".overlay"),buttonClose=modal.querySelector(".close"),login=modal.querySelector('input[type="text"]'),email=modal.querySelector('input[type="email"]'),comment=modal.querySelector("textarea");buttonOpen.addEventListener("click",function(e){e.preventDefault(),modal.classList.add("content-show"),modalBackground.classList.add("overlay-appear"),login.focus()}),window.addEventListener("keydown",function(e){27===e.keyCode?(modal.classList.contains("content-show")&&modal.classList.remove("content-show"),modalBackground.classList.remove("overlay-appear"),modal.classList.remove("feedback-form-invalid")):13===e.keyCode&&(login.value&&email.value&&comment.value||(modal.classList.remove("feedback-form-invalid"),modal.offsetWidth=modal.offsetWidth,e.preventDefault(),modal.classList.add("feedback-form-invalid")))}),buttonClose.addEventListener("click",function(e){e.preventDefault(),modal.classList.remove("content-show"),modalBackground.classList.remove("overlay-appear"),modal.classList.remove("feedback-form-invalid")}),modal.addEventListener("submit",function(e){login.value&&email.value&&comment.value||(modal.classList.remove("feedback-form-invalid"),modal.offsetWidth=modal.offsetWidth,e.preventDefault(),modal.classList.add("feedback-form-invalid"))});
+var buttonOpen = document.querySelector('.main-contacts .btn');
+var modal = document.querySelector('.feedback-form');
+var modalBackground = document.querySelector('.overlay');
+var buttonClose = modal.querySelector('.close');
+
+var login = modal.querySelector('input[type="text"]');
+var email = modal.querySelector('input[type="email"]');
+var comment = modal.querySelector('textarea');
+
+buttonOpen.addEventListener('click', function(event) {
+  event.preventDefault();
+  modal.classList.add('content-show');
+  modalBackground.classList.add('overlay-appear');
+  login.focus();
+});
+
+window.addEventListener('keydown', function(event) {
+  if(event.keyCode === 27) {
+    if(modal.classList.contains('content-show'))
+    modal.classList.remove('content-show');
+    modalBackground.classList.remove('overlay-appear');
+    modal.classList.remove('feedback-form-invalid');
+    } else if(event.keyCode === 13) {
+      if(!login.value || !email.value || !comment.value) {
+        modal.classList.remove('feedback-form-invalid');
+        modal.offsetWidth = modal.offsetWidth;
+        event.preventDefault();
+        modal.classList.add('feedback-form-invalid');
+      };
+    };
+});
+
+buttonClose.addEventListener('click', function(event) {
+  event.preventDefault();
+  modal.classList.remove('content-show');
+  modalBackground.classList.remove('overlay-appear');
+  modal.classList.remove('feedback-form-invalid');
+});
+
+modal.addEventListener('submit', function(event) {
+  if(!login.value || !email.value || !comment.value) {
+    modal.classList.remove('feedback-form-invalid');
+    modal.offsetWidth = modal.offsetWidth;
+    event.preventDefault();
+    modal.classList.add('feedback-form-invalid');
+  }
+});
