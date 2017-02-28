7bd03e8b197fb675d12bdcd391a330dfd03ff45b
Maxim Voropai <muckam49@mail.ru>
2017-02-28 13:10:15 +0800

    icom map changed

--- js/popup.js
+++ js/popup.js
@@ -45,3 +45,25 @@ modal.addEventListener('submit', function(event) {
     modal.classList.add('feedback-form-invalid');
   }
 });
+
+ymaps.ready(function() {
+  var myMap = new ymaps.Map('map', {
+      center: [59.938536, 30.3224549],
+      zoom: 16
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
+    myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      hintContent: 'Gllacy',
+      balloonContent: 'Раздаем мороженки'
+    }, {
+      iconLayout: 'default#image',
+      iconImageHref: '../img/ice_cream_icon.png',
+      iconImageSize: [218, 142],
+      iconImageOffset: [-28, -142]
+    });
+  myMap.behaviors.disable('scrollZoom');
+  myMap.geoObjects.add(myPlacemark);
+});
+
+
